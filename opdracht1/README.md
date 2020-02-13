b8e37dbe8451dd0c8eef8944cfb8d1c74238dc0c
ninoelchico21 <58262860+ninoelchico21@users.noreply.github.com>
2020-02-13 14:44:19 +0100

    Tweede versie film
    In deze versie heb ik vooral de css aangepast door een heart-togglebuton te maken. Verder heb ik ook iets geprobeerd met javascript

--- opdracht1/README.md
+++ opdracht1/README.md
@@ -1,3 +1,17 @@
+# Procesverslag Frontend voor Designers
+
+Eerste versie film:
+
+Tweede versie film:
+
+Derde versie (na feedback) versie:
+
+
+
+
+
+
+
 # Frontend voor Designers - opdracht 1: Een Micro-interactie uitwerken en testen
 
 Ontwerp een user interface voor een gegeven use case. Werk je ontwerp uit in HTML, CSS en Javascript om te kunnen testen in een browser.
--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -4,3 +4,17 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+
+// Variabelen opstellen: select the HTML elements which you want to manipulate
+var emojiHeart = document.querySelector('.emoji-heart');
+
+// Hier hang ik een oortje aan de ... die checkt of er een klik plaatsvind.
+// Bij een click wordt de functie verzinfunctie gerunned.
+emojiHeart.addEventListener('click', verzinFunctie);
+
+// Deze functie toggled de classes cssselector1 en cssselector2. Deze kun je in css vinden.
+function verzinFunctie() {
+    emojiHeart.classList.toggle('active');
+    emojiHeart.classList.toggle('active');
+}
