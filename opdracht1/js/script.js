7a7255a4cabed4cf831d0e7dedfb222921d393da
ninoelchico21 <58262860+ninoelchico21@users.noreply.github.com>
2020-02-26 16:42:33 +0100

    Derde versie film (na de feedback)
    In de derde versie ben ik me vooral gaan focussen op de js. Hiebij heb ik de heart toggle button weggehaald en dat vervangen met een zwarte hart svg die je nu rood kan klikken. De nav is weer lelijk geworden, maar dat ga ik nog fiksen.

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -6,15 +6,49 @@
 */
 
 
+//
+//var films = ["matrix_the_1999.jpg", "scary_movie_5.jpg", "wall-e_2008.jpg", "shutter-island.jpg", "x-men_the_last_stand.jpg"];
+//
+//console.log(films.length);
+//// 5
+//
+//
+//var first = films[0]; //matrix_the_1999.jpg
+//var second = films[films.length - 4]; //scary_movie_5.jpg
+//var third = films[films.length - 3]; // wall-e_2008.jpg
+//var fourth = films[films.length - 2]; //shutter-island.jpg
+//var last = films[films.length - 1]; // x-men_the_last_stand.jpg
+
+
+
 // Variabelen opstellen: select the HTML elements which you want to manipulate
-var emojiHeart = document.querySelector('.emoji-heart');
+var navHart = document.getElementById('navlinkhart');
+var blackHart = document.getElementsByClassName('blackhearts');
+
+console.log(blackHart[0]);
+
+
+console.log(navHart);
+
+
 
 // Hier hang ik een oortje aan de ... die checkt of er een klik plaatsvind.
 // Bij een click wordt de functie verzinfunctie gerunned.
-emojiHeart.addEventListener('click', verzinFunctie);
+navHart.addEventListener('click', showLiked);
+blackHart[0].addEventListener('click', blackheartClicked);
+
+for (i = 0; i < blackHart.length; i++) {
+    blackHart[i].addEventListener('click', blackheartClicked);
+}
+
 
 // Deze functie toggled de classes cssselector1 en cssselector2. Deze kun je in css vinden.
-function verzinFunctie() {
-    emojiHeart.classList.toggle('active');
-    emojiHeart.classList.toggle('active');
+function showLiked() {
+    console.log("werkt");
+
+}
+
+function blackheartClicked() {
+    console.log(this);
+    this.classList.toggle('redhart');
 }
