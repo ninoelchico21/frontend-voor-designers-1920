a365c27557a8f35447c4ae29cb9b6008df016449
ninoelchico21 <58262860+ninoelchico21@users.noreply.github.com>
2020-03-11 20:24:17 +0100

    carousel versie 2
    in deze versie heb ik de nav buttons geplaatst en de css aangepast. Verder heb ik nog js toegevoegd.

--- opdracht2/js/script.js
+++ opdracht2/js/script.js
@@ -1,24 +1,42 @@
 //  Hier maak ik variabelen aan en selecteer ik de twee knoppen uit de html document
-var vorigeKnop = document.querySelector('.vorige');
-var volgendeKnop = document.querySelector('.volgende');
-var images =
+//var vorigeKnop = document.querySelector('.vorige');
+//var volgendeKnop = document.querySelector('.volgende');
+//var links = document.querySelectorAll('#link .links');
+var bolletje = document.getElementsByClassName('bol');
 
-    //  Vervolgens hang ik een oortje aan beide knoppen om te luisteren naar en click,
-    //  Zodat dan de bijbehorende functies worden getriggerd.
-    vorigeKnop.addEventListener('click', nextLink);
-volgendeKnop.addEventListener('click', previousLink);
+console.log(bolletje);
 
+//  Vervolgens hang ik een oortje aan beide knoppen om te luisteren naar en click,
+//  Zodat dan de bijbehorende functies worden getriggerd.
+//vorigeKnop.addEventListener('click', nextLink);
+//volgendeKnop.addEventListener('click', previousLink);
 
-//  In de functie wil ik zeggen dat de volgende link moet worden geshowed
-function nextlink() {
-    console.log("werkt");
+for (i = 0; i < bolletje.length; i++) {
+    console.log(bolletje[i]);
 
-    document.querySelector('');
+    bolletje[i].addEventListener('click', bolletjeZwart);
 
+    function bolletjeZwart() {
+        document.querySelector(".zwart").classList.remove("zwart");
+        this.classList.add('zwart');
+    }
 }
 
 
-//  In de functie wil ik zeggen dat de vorige link moet worden geshowed
-function previouslink() {
-    console.log("werkt");
-}
+
+
+
+
+////  In de functie wil ik zeggen dat de volgende link moet worden geshowed
+//function nextLink() {
+//    console.log("werkt");
+//
+//    document.querySelector('#link1');
+//    document.querySelector('#link2');
+//}
+//
+//
+////  In de functie wil ik zeggen dat de vorige link moet worden geshowed
+//function previousLink() {
+//    console.log("werkt");
+//}
